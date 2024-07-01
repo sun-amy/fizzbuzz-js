015be1b0e9c2649c0ba80c65bf86e730daffc052
Amy Sun <amy.sun@softwire.com>
2024-07-01 15:26:02 +0100

    Added Fizz, Buzz and FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,22 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Hello World!');
+    for (let n = 1; n <= 100; n ++) {
+        if (n % 3 === 0 && n % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (n % 3 === 0) {
+            console.log("Fizz");
+        } else if (n % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(n);
+        }
+        
+        }
+    }
 
     // Put your code here...
 
     
-}
-
 // Now we run the main function...
 fizzbuzz();
